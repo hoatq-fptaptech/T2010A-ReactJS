c926aefc309d0cfa196d6a60793722bacbdef6f7
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2022-02-14 15:58:56 +0700

    state - form

--- src/App.js
+++ src/App.js
@@ -5,33 +5,88 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 class App extends React.Component{
-  ready(data){
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOfPerson:0,
+            list: [
+                {
+                    productName: "Áo lông thời trang",
+                    price: 15.9
+                },
+                {
+                    productName: "Áo khoác thời trang",
+                    price: 10.9
+                },
+                {
+                    productName: "Túi xách Gucci",
+                    price: 108
+                },
+                {
+                    productName: "Đồng hồ Casio",
+                    price: 6
+                }
+            ],
+            product:{
+                productName:"",
+                price:0
+            }
+        }
+        this.changeNum = this.changeNum.bind(this);
+        this.formSubmit = this.formSubmit.bind(this);
+        this.changeValue = this.changeValue.bind(this);
+    }
+    changeNum(){
+        let n = this.state.numberOfPerson;
+        this.setState({numberOfPerson:n+1});
+    }
+    ready(data){
       alert(data+": I am ready!");
-  }
+    }
+    // form handling
+    formSubmit(e){
+        let p = this.state.product;
+        if(p.productName.length > 0 && p.price>=0){
+            let list = this.state.list;
+            list.push(p);
+            this.setState({
+                list:list,
+                product:{
+                    productName:"",
+                    price:0
+                }
+            });
+        }else{
+            alert("Vui lòng nhập đủ thông tin sản phẩm!");
+        }
+        e.preventDefault();
+    }
+    changeValue(e){
+        let p = this.state.product;
+        p[e.target.name] = e.target.value;
+        this.setState({product:p});
+    }
   render(){
-      var list = [
-          {
-              productName: "Áo lông thời trang",
-              price: 15.9
-          },
-          {
-              productName: "Áo khoác thời trang",
-              price: 10.9
-          },
-          {
-              productName: "Túi xách Gucci",
-              price: 108
-          },
-          {
-              productName: "Đồng hồ Casio",
-              price: 6
-          }
-      ];
+      var list = this.state.list;
     return (
         <div className="App">
             <Header/>
             <div className="container">
-                <Main list={list} ready={this.ready}/>
+                <form onSubmit={this.formSubmit}>
+                    <div className="form-group">
+                        <label>Product Name</label>
+                        <input onChange={this.changeValue} name="productName" value={this.state.product.productName} className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Price</label>
+                        <input onChange={this.changeValue} name="price"  value={this.state.product.price} className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-danger">Submit</button>
+                    </div>
+                </form>
+                <button onClick={this.changeNum}>Change</button>
+                <Main num={this.state.numberOfPerson} list={list} ready={this.ready}/>
             </div>
 
             <Footer/>
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,20 +2,52 @@ import React from "react";
 import Product from "./Product";
 
 export default class Main extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOfPerson: props.num
+        }
+        this.changeNumber = this.changeNumber.bind(this);
+    }
     startReady(data){
         this.props.ready(data);
     }
+    componentWillMount() {
+        console.log("will mount");
+    }
+    componentDidMount() {
+        console.log("did mount..");
+    }
+    changeNumber(){
+        let n = this.state.numberOfPerson;
+        n++;
+        this.setState({numberOfPerson:n});
+    }
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        console.log("will update...");
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("Did update..");
+    }
     render(){
+        console.log("render...");
         var products = this.props.list;
-        return <ul className="row">
-            {
-              products.map((e,k)=>{
-                  //e <=> products[i]
-                 return <li className="col-md-3" onClick={this.startReady.bind(this,e.productName)} key={k}>
-                         <Product productName={e.productName} price={e.price}/>
-                     </li>
-              })
-            }
-        </ul>
+        return (
+        <div>
+            <h1>App: {this.props.num}</h1>
+            <h1>Main: {this.state.numberOfPerson}</h1>
+            <button onClick={this.changeNumber} className="btn btn-primary">Change</button>
+            <ul className="row">
+                {
+                  products.map((e,k)=>{
+                      //e <=> products[i]
+                     return <li className="col-md-3" onClick={this.startReady.bind(this,e.productName)} key={k}>
+                             <Product productName={e.productName} price={e.price}/>
+                         </li>
+                  })
+                }
+            </ul>
+        </div>
+        )
     }
 }
